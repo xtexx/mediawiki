50164d18091dd950ad6e04d8609a1e1571e0cc52
Peter Hedenskog <phedenskog@wikimedia.org>
2026-02-17 13:44:57 +0100

    selenium: Split page into admin and user scenarios
    By splitting the test suites to shorter suites, we can more easily run tests in parallel in CI.
    
    Bug: T417535
    Change-Id: I68e9604ea79a6f2fb8180dd0322b3a38eddaed27

--- tests/selenium/specs/page.js
+++ tests/selenium/specs/page.js
@@ -1,13 +1,9 @@
 import BlankPage from 'wdio-mediawiki/BlankPage.js';
 import { createApiClient } from 'wdio-mediawiki/Api.js';
-import DeletePage from '../pageobjects/delete.page.js';
-import RestorePage from '../pageobjects/restore.page.js';
 import EditPage from '../pageobjects/edit.page.js';
 import HistoryPage from '../pageobjects/history.page.js';
 import UndoPage from '../pageobjects/undo.page.js';
-import ProtectPage from '../pageobjects/protect.page.js';
 import LoginPage from 'wdio-mediawiki/LoginPage.js';
-import LogoutPage from 'wdio-mediawiki/LogoutPage.js';
 import { getTestString, isTargetNotWikitext } from 'wdio-mediawiki/Util.js';
 
 describe( 'Page', () => {
@@ -87,56 +83,6 @@ describe( 'Page', () => {
 		// check
 		await HistoryPage.open( name );
 		await expect( HistoryPage.comment ).toHaveText( `created with "${ content }"` );
-	} );
-
-	it( 'should be deletable', async () => {
-		// create
-		await apiClient.edit( name, content, 'create for delete' );
-
-		// login
-		await LoginPage.loginAdmin();
-		// delete
-		await DeletePage.delete( name, 'delete reason' );
-
-		// check
-		await expect( DeletePage.displayedContent ).toHaveText( expect.stringContaining( `"${ name }" has been deleted.` ) );
-	} );
-
-	it( 'should be restorable', async () => {
-		// create and delete
-		await apiClient.edit( name, content, 'create for delete' );
-		await apiClient.delete( name, 'delete for restore' );
-
-		// login
-		await LoginPage.loginAdmin();
-
-		// restore
-		await RestorePage.restore( name, 'restore reason' );
-
-		// check
-		await expect( RestorePage.displayedContent ).toHaveText( expect.stringContaining( `${ name } has been undeleted` ) );
-	} );
-
-	it( 'should be protectable', async () => {
-
-		await apiClient.edit( name, content, 'create for protect' );
-
-		// login
-		await LoginPage.loginAdmin();
-
-		await ProtectPage.protect(
-			name,
-			'protect reason',
-			'Allow only administrators'
-		);
-
-		// Logout
-		await LogoutPage.logout();
-
-		// Check that we can't edit the page anymore
-		await EditPage.openForEditing( name );
-		await expect( EditPage.save ).not.toExist();
-		await expect( EditPage.heading ).toHaveText( `View source for ${ name }` );
 	} );
 
 	it( 'should be undoable', async () => {
---
+++ tests/selenium/specs/pageadmin.js
@@ -0,0 +1,81 @@
+import BlankPage from 'wdio-mediawiki/BlankPage.js';
+import { createApiClient } from 'wdio-mediawiki/Api.js';
+import DeletePage from '../pageobjects/delete.page.js';
+import RestorePage from '../pageobjects/restore.page.js';
+import EditPage from '../pageobjects/edit.page.js';
+import ProtectPage from '../pageobjects/protect.page.js';
+import LoginPage from 'wdio-mediawiki/LoginPage.js';
+import LogoutPage from 'wdio-mediawiki/LogoutPage.js';
+import { getTestString, isTargetNotWikitext } from 'wdio-mediawiki/Util.js';
+
+describe( 'Page admin actions', () => {
+	let content, name, apiClient;
+
+	before( async () => {
+		apiClient = await createApiClient();
+	} );
+
+	beforeEach( async function () {
+		await browser.deleteAllCookies();
+		content = getTestString( 'beforeEach-content-' );
+		name = getTestString( 'BeforeEach-name-' );
+
+		// First try to load a blank page, so the next command works.
+		await BlankPage.open();
+		// Don't try to run wikitext-specific tests if the test namespace isn't wikitext by default.
+		if ( await isTargetNotWikitext( name ) ) {
+			this.skip();
+		}
+	} );
+
+	it( 'should be deletable', async () => {
+		// create
+		await apiClient.edit( name, content, 'create for delete' );
+
+		// login
+		await LoginPage.loginAdmin();
+		// delete
+		await DeletePage.delete( name, 'delete reason' );
+
+		// check
+		await expect( DeletePage.displayedContent ).toHaveText( expect.stringContaining( `"${ name }" has been deleted.` ) );
+	} );
+
+	it( 'should be restorable', async () => {
+		// create and delete
+		await apiClient.edit( name, content, 'create for delete' );
+		await apiClient.delete( name, 'delete for restore' );
+
+		// login
+		await LoginPage.loginAdmin();
+
+		// restore
+		await RestorePage.restore( name, 'restore reason' );
+
+		// check
+		await expect( RestorePage.displayedContent ).toHaveText( expect.stringContaining( `${ name } has been undeleted` ) );
+	} );
+
+	it( 'should be protectable', async () => {
+
+		await apiClient.edit( name, content, 'create for protect' );
+
+		// login
+		await LoginPage.loginAdmin();
+
+		await ProtectPage.protect(
+			name,
+			'protect reason',
+			'Allow only administrators'
+		);
+
+		// Logout
+		await LogoutPage.logout();
+
+		// Check that we can't edit the page anymore
+		await EditPage.openForEditing( name );
+		await expect( EditPage.save ).not.toExist();
+		await expect( EditPage.heading ).toHaveText( `View source for ${ name }` );
+	} );
+
+} );
